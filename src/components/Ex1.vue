55b4e31300eba15be452a66c0446efe0a6df7c21
yixiang07 <154117005+yixiang07@users.noreply.github.com>
2025-10-22 01:09:04 +0000

    Ex1 done

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,7 +2,27 @@
     export default { 
         // Add Code Here to complete the task
         // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
-    }
+        data(){
+          return {
+            x: 0,
+            y: 0,
+            selectedOp:"",
+            operators: ["+", "-", "*", "/", "%"],
+          };
+        },
+        computed: {
+          result() {
+            switch(this.selectedOp) {
+            case '+': return this.x + this.y;
+            case '-': return this.x - this.y;
+            case '*': return this.x * this.y;
+            case '/': return this.y !== 0 ? this.x / this.y : 'Error: ÷0';
+            case '%': return this.y !== 0 ? this.x % this.y : 'Error: ÷0';
+            default: return 0;
+            }
+          }
+        }
+        }
 </script>
 
 <template>
