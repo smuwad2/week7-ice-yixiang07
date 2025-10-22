5988af64707c9d0875f30e1890e81380a21ad10c
yixiang07 <154117005+yixiang07@users.noreply.github.com>
2025-10-22 02:31:35 +0000

    ex2 done

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,11 +1,25 @@
 <script>
+import { Key } from 'react';
+
     export default { 
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: ""
             }
             
+        },
+        methods: {
+          addItem() {
+            if (this.newItem.trim() !== '') {
+              this.items.push(this.newItem.trim());
+              this.newItem = '';
+            }
+          },
+          deleteItem(index) {
+            this.items.splice(index, 1);
+          }
         }
     }
 </script>
@@ -14,5 +28,12 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{item}}<button @click="deleteItem(index)">Delete!</button>
+      </li>
+    </ul>
+    <input type="text" v-model="newItem"></input>
+    <button @click="addItem()">Add</button>
    
 </template>
